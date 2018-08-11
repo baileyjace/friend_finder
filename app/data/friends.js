ff98b6febf4619b9e75710da8c7366587bee9168
Bailey Jace <baileyjace@gmail.com>
2018-08-11 17:07:59 -0500

    ok

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+friendsArray = [
+    {
+		name: 'Finn',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/f/f3/Original_Finn.png/revision/latest?cb=20120921151658',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Jake',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3b/Jakesalad.png/revision/latest?cb=20160503014517',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Princess Bubblegum',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/00/Princess_Bubblegum.png/revision/latest?cb=20120921151236',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Ice King',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/6/64/Original_Ice_King.png/revision/latest?cb=20160405041324',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Marceline',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/e0/Marceline_Stock_Night.png/revision/latest?cb=20121003133650',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'BMO',
+        photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/8/81/BMO.png/revision/latest?cb=20161024201513',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Lumpy Space Princess',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/9/9f/Lumpy_Space.png/revision/latest?cb=20110730224431',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'Peppermint Butler',
+		photo: 'https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/5/51/Peppermint_Butler_hissing.jpg/revision/latest?cb=20110504011033',
+		scores: [
+			
+		]
+    },
+    {
+		name: 'The Snail',
+		photo: 'http://cdn.shopify.com/s/files/1/1697/2541/products/advtime_snail_decal2_grande.jpg?v=1499300445',
+		scores: [
+			
+		]
+	},
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,67 @@
+var path = require("path");
+
+// linking the data from the friends.js file where we have all our friends 
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+
+    // get function grabbing the array of friends
+    app.get("/data/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    // post function where we take user's input and calculate the difference
+    // between user's score and friend's score
+    app.post("data/friends", function(req, res) {
+
+        // user's input
+        var userInput = req.body;
+
+        // user's scores
+        var userResponse = userInput.scores;
+
+        // friend with the best score
+        var friendName = "";
+        var friendImage = "";
+
+        // setting difference super high for comparison after we calculate
+        // user's score
+        var totalDiff = 1000;
+
+        // pulls all friends from the array
+        for (var i =0; i < friends.length; i++) {
+
+            // starting point for calculating score of questions
+            var diff = 0;
+
+            // Math.abs() to calculate the absolute value of the friend matches'
+            // scores and subtracting the user's score, leaving the best friend
+            // match as the lowest value
+            for (var j = 0; j < userResponse.length; j++) {
+                diff += Math.abs(friends[i].score[j] - userResponse[j]);
+            }
+
+            console.log("difference = " + diff);
+
+            // logging the results of the calculation and saving them to the variables
+            if (diff < totalDiff) {
+
+                console.log("Your closest friend match: " + diff);
+                console.log("Friend name: " + friends[i].name);
+                console.log("Friend image: " + friends[i].photo);
+
+                totalDiff = diff;
+                friendName = friends[i].name;
+                friendImage = friends[i].photo;
+            };
+        };
+
+        // add new user
+        friends.push(userInput);
+
+        // send response
+        res.json({status: "ok", friendName: friendImage, friendImage: friendImage});
+
+
+    });
+};
